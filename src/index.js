ff53faf82574094ce09bc94fb24d5ceba8940477
miamam1 <theshpeecrab@gmail.com>
2024-02-23 13:49:03 +0000

    Added: - Basic HTML skeleton code - Basic webpack skeleton code with required dependencies - Added ability to input for weather locations - Added some basic validation if the user doesn't type in a known location To do: - Extract relevant information, as currently the response is everything, which is not needed - Showcase information on webpage instead of console

---
+++ src/index.js
@@ -0,0 +1,52 @@
+import "./style.css";
+
+function handleError(fn) {
+  return function (...params) {
+    return fn(...params).catch(function (error) {
+      console.log("YOUR STUPID, HERES WHY:", error);
+    });
+  };
+}
+
+
+async function getWeather(place) {
+  const response = await fetch(
+    `http://api.weatherapi.com/v1/forecast.json?key=f5898a6f5ccf4851aa9151936241002&q=${place}&days=3`,
+    { mode: "cors" }
+  )
+    //   im stupid, its fine for the errors to show in console literally does for all the top ones, just make sure you show the user via UI
+     if( !response.ok) {
+        return false
+    } else {
+        const responseData = await response.json();
+        return responseData;
+}
+    }
+
+
+
+    
+
+const safeGetWeather = handleError(getWeather);
+
+console.log(await safeGetWeather("London"));
+
+const input = document.createElement("input");
+document.body.append(input);
+ input.addEventListener("keypress", (value) => {
+  const duration = 1000;
+  clearTimeout(input._timer);
+  input._timer = setTimeout(async () => {
+    
+    if(await safeGetWeather(input.value) === false) {
+        input.style.backgroundColor = "red"
+    } else {
+        const place = await safeGetWeather(input.value)
+        // some of the propertie names are weird and underscores and i cant access them cus cus it shoiws undefined, need to find the correct property names prolly on documentation 
+        // console.log(item.current.humidity)
+        // key downs are weird sometime could be better to do an submit button that you can activate via the enter key like the top examp 
+        console.log(place)
+        input.style.backgroundColor = "white"
+    }
+  }, duration);
+});
